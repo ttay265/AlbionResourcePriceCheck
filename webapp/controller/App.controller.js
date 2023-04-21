ece2c47bf6e23e74e573d93b4cd559b1837ef754
quoctuan.ngo <quoc-tuan.ngo@teamwork.net>
2023-04-22 06:09:01 +0700

    v1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,15 @@
 sap.ui.define([
-	// "sap/ui/core/mvc/Controller",
-	"com/order/productCustom_Order_Product/controller/BaseController",
-	"sap/m/Dialog"
+    // "sap/ui/core/mvc/Controller",
+    "com/order/productCustom_Order_Product/controller/BaseController",
+    "sap/m/Dialog"
 ], function(BaseController, Dialog) {
-	"use strict";
+    "use strict";
 
-	return BaseController.extend("com.order.productCustom_Order_Product.controller.App", {
-		
-	});
+    return BaseController.extend("com.order.productCustom_Order_Product.controller.App", {
+        // apply content density mode to root view
+        onInit: function() {
+
+            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+        }
+    });
 });
